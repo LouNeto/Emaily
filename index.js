fd211bac70407e11b67a1a73067f37daa0b10819
Lou-Gomes Neto <lou.g.n@hotmail.com>
2020-09-02 14:10:53 -0400

    Forgot save on passport use google strategy line

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
+passport.use(new GoogleStrategy());
+
 // Dynamic port binding for Heroku
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
