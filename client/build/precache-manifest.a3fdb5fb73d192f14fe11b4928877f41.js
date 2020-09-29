4d003e1300de59cc485a174bdcf20a490aa61c73
Lou-Gomes Neto <lou.g.n@hotmail.com>
2020-09-29 16:13:40 -0400

    pushing build to heroku

---
+++ client/build/precache-manifest.a3fdb5fb73d192f14fe11b4928877f41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e800f9255037efc43ff55a459d80fb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "001de8fa3773b531e75a",
+    "url": "/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "001de8fa3773b531e75a",
+    "url": "/static/js/2.6cd7781a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6cd7781a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67813e2eb8e84f6f100b",
+    "url": "/static/js/main.35542500.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
